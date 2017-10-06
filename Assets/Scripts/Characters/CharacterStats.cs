6aa637e20096f0fce58bd74c135ae14fb60ac417
Unknown <tythin4@gmail.com>
2017-10-05 19:49:26 -0700

    empty scene and character stats

---
+++ Assets/Scripts/Characters/CharacterStats.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour
+{
+
+    public int Hp;
+    public int Mana;
+
+    public int Attack;
+    public int Defense;
+
+    public int Speed;
+
+}
