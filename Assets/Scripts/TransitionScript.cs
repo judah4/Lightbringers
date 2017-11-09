613960652d71e48439f9eb35947cf6bd2f4a3ded
Unknown <reperez79@gmail.com>
2017-11-08 22:57:03 -0800

    Added ability to change scenes by approaching location

---
+++ Assets/Scripts/TransitionScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class TransitionScript : MonoBehaviour
+{
+
+    public Vector3 LoadLocation; //holds the position the player will spawn in the next location
+    public string SceneName;
+
+
+
+    void OnTriggerEnter(Collider other) //while player model is inside the object
+    {
+        //checks to see that it was the player that is in transition area and not something else
+        if (other.CompareTag("Player")) 
+        {
+            print("inside change location");
+
+
+            //insert code here to show UI thing to prompt user to interact
+            //interactionUIThing.enabled = true;
+
+            if (Input.GetButton("Submit"))
+            {
+                SceneManager.LoadScene(SceneName);
+
+                print(("tried to switch to " + SceneName));
+            }
+        }
+        
+    }
+
+    void OnTriggerExit(Collider other) //while player model is leaves the object
+    {
+        if (other.CompareTag("Player"))
+        {
+            print("left change location");
+            //hide UI thing for User to interact
+            //interactionUIThing.enabled = false;
+
+        }
+    }
+
+}
