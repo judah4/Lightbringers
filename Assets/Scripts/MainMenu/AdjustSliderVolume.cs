795b6d44e613bf528831b8d32f8727d177429b83
Jonathan Tapia <tapiaxj@gmail.com>
2017-11-09 14:50:24 -0800

    Connected Main Menu volume sliders to audio system

---
+++ Assets/Scripts/MainMenu/AdjustSliderVolume.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdjustSliderVolume : MonoBehaviour {
+
+	public Slider mainSlider;
+	public Slider[] childSliders; 
+
+	void Start () {
+		mainSlider.onValueChanged.AddListener(delegate { UpdateChildSliders(); });
+	}
+
+	private void UpdateChildSliders()
+	{
+		foreach (var child in childSliders)
+		{
+			child.value = mainSlider.value;
+		}
+	}
+}
