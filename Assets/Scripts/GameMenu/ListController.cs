07b9f8aede5a824e2b1dd7fd23c33d105cbbf3ed
Jonathan Tapia <tapiaxj@gmail.com>
2017-11-18 17:35:25 -0800

    Started preliminary work on inventory manager and ui

---
+++ Assets/Scripts/GameMenu/ListController.cs
@@ -0,0 +1,30 @@
+﻿using Assets.Scripts.Items;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListController : MonoBehaviour {
+
+	public InventoryManager Inventory;
+	public GameObject ItemUiPrefab;
+	public GameObject ContentPanel;
+
+	void Start()
+	{
+		UpdateInventoryUI();
+	}
+
+	private void UpdateInventoryUI()
+	{
+		foreach(Item item in Inventory.items)
+		{
+			GameObject itemPanel = Instantiate(ItemUiPrefab) as GameObject;
+			ItemController controller = itemPanel.GetComponent<ItemController>();
+
+			controller.itemName.text = item.itemName;
+
+			//itemPanel.transform.parent = ContentPanel.transform;
+			itemPanel.transform.SetParent(ContentPanel.transform);
+		}
+	}
+}
