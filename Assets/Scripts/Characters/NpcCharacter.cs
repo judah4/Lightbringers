005e598af21996bfeea02c7bfbb699f3f93220e2
Judah Perez <tythin4@gmail.com>
2017-11-18 16:50:19 -0800

    simple npcs loaded

---
+++ Assets/Scripts/Characters/NpcCharacter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcCharacter : MonoBehaviour {
+
+    public int ModelId = 2;
+
+    [SerializeField]
+    AnimatedCharacter character;
+
+
+    void Start()
+    {
+        character = CharacterList.Instance.Load(ModelId, transform);
+    }
+	
+
+}
