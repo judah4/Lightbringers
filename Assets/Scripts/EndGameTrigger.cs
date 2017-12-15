a63eb01e70f530878810c30a0d0ec3e790b50572
Judah Perez <tythin4@gmail.com>
2017-12-14 23:48:00 -0800

    Added an win scene

---
+++ Assets/Scripts/EndGameTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts.World;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGameTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter(Collider other)
+        {
+            var chControl = other.gameObject.GetComponent<CharacterController>();
+            if (WorldStateManager.Instance.Events.Contains(500))
+            {
+                
+                SceneManager.LoadScene("WinScene");
+            }
+        }
+}
