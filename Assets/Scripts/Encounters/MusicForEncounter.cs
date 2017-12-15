8fb4c56ec52598f3a4e694610c1219937f073cb2
Judah Perez <tythin4@gmail.com>
2017-12-14 22:54:39 -0800

    music for encounters. dragon and king

---
+++ Assets/Scripts/Encounters/MusicForEncounter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts.Encounters;
+using UnityEngine;
+
+public class MusicForEncounter : MonoBehaviour
+{
+
+    public List<AudioClip> clips;
+    public AudioSource Source;
+
+    public BasicEncounterSetup EncounterSetup;
+
+	// Use this for initialization
+	void Start () {
+	    if (EncounterSetup.EncounterId == 13)
+	    {
+	        Source.clip = clips[0];
+            Source.Play();
+	    }
+        else if (EncounterSetup.EncounterId == 14)
+	    {
+	        Source.clip = clips[1];
+            Source.Play();
+	    }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
